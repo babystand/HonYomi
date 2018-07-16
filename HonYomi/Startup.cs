504a02bdbe97719bad632d53a47d680175f44297
Unknown <vypon3000@gmail.com>
2018-07-16 14:09:49 -0400

    fixed content root + formatting startup.cs

--- HonYomi/Startup.cs
+++ HonYomi/Startup.cs
@@ -15,6 +15,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.IdentityModel.Tokens;
 
 namespace HonYomi
@@ -22,6 +23,7 @@ namespace HonYomi
     public class Startup
     {
         public static int port = 5000;
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -30,37 +32,37 @@ namespace HonYomi
             services.AddDbContext<HonyomiContext>();
             /***Identity***/
             services.AddIdentity<IdentityUser, IdentityRole>(options =>
-                                                             {
-                                                                 options.Password.RequireNonAlphanumeric = false;
-                                                                 options.Password.RequiredLength = 8;
-                                                                 options.Password.RequireUppercase = true;
-                                                                 options.Password.RequireLowercase = true;
-                                                                 options.Password.RequireDigit = false;
-                                                                 options.Password.RequiredUniqueChars = 5;
-                                                             }).AddEntityFrameworkStores<HonyomiContext>()
-                    .AddDefaultTokenProviders();
+                {
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequiredLength         = 8;
+                    options.Password.RequireUppercase       = true;
+                    options.Password.RequireLowercase       = true;
+                    options.Password.RequireDigit           = false;
+                    options.Password.RequiredUniqueChars    = 5;
+                }).AddEntityFrameworkStores<HonyomiContext>()
+                .AddDefaultTokenProviders();
             services.AddTransient<UserManager<IdentityUser>>();
             /***JWT config***/
             //clear defaults
             JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
             services.AddAuthentication(options =>
-                                       {
-                                           options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                                           options.DefaultScheme             = JwtBearerDefaults.AuthenticationScheme;
-                                           options.DefaultChallengeScheme    = JwtBearerDefaults.AuthenticationScheme;
-                                       }).AddJwtBearer(cfg =>
-                                                       {
-                                                           cfg.RequireHttpsMetadata = false;
-                                                           cfg.SaveToken            = true;
-                                                           cfg.TokenValidationParameters =
-                                                               new TokenValidationParameters()
-                                                               {
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme             = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme    = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(cfg =>
+            {
+                cfg.RequireHttpsMetadata = false;
+                cfg.SaveToken            = true;
+                cfg.TokenValidationParameters =
+                    new TokenValidationParameters()
+                    {
 //                                                                   ValidIssuer      = "ISSUER",
 //                                                                   ValidAudience    = "ISSUER",
-                                                                   IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("key")),
-                                                                   ClockSkew        = TimeSpan.Zero
-                                                               };
-                                                       });
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("key")),
+                        ClockSkew        = TimeSpan.Zero
+                    };
+            });
             /***MVC***/
             services.AddMvc();
             /***Scheduled Background Tasks (Hangfire)***/
@@ -68,28 +70,40 @@ namespace HonYomi
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, HonyomiContext dbContext, UserManager<IdentityUser> uMan)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, HonyomiContext dbContext,
+            UserManager<IdentityUser>             uMan)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            /***MVC***/
             app.UseMvc();
-            app.UseStaticFiles();
+
+            /***Configure static files at root address "ui" instead of "wwwroot/dist" ***/
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider =
+                    new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "dist")),
+                RequestPath = "/ui"
+            });
+            
+            /***Add Hangfire for Background tasks***/
             app.UseHangfireDashboard();
             app.UseHangfireServer();
 
-            //true if database had to be created
+            /***Create DB and populate defaults if needed***/
             if (dbContext.Database.EnsureCreated())
             {
                 dbContext.CreateDefaults(uMan).Wait();
             }
-            HonyomiConfig config = dbContext.Configs.First();
-                port = config.ServerPort;
-            
-            RecurringJob.AddOrUpdate("scan", () => DirectoryScanner.ScanWatchDirectories(),  Cron.MinuteInterval(config.ScanInterval));
 
+            HonyomiConfig config = dbContext.Configs.First();
+            port = config.ServerPort;
+
+            RecurringJob.AddOrUpdate("scan", () => DirectoryScanner.ScanWatchDirectories(),
+                                     Cron.MinuteInterval(config.ScanInterval));
         }
     }
 }
