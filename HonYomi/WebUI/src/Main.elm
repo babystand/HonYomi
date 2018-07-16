7ecdf1b5ca17d673633ca62a3678ec0394cabb6a
Unknown <vypon3000@gmail.com>
2018-07-16 14:09:49 -0400

    fixed content root + formatting startup.cs

--- HonYomi/WebUI/src/Main.elm
+++ HonYomi/WebUI/src/Main.elm
@@ -4,25 +4,46 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 
-type alias Model = Int
 
-init : (Model, Cmd Msg)
-init = (0, Cmd.none)
+type alias Page =
+    Int
 
-type Msg = Inc
-update : Msg -> Model -> (Model, Cmd Msg)
+
+type alias User =
+    { username : String
+    , token : String
+    }
+
+
+type alias Model =
+    Int
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( 0, Cmd.none )
+
+
+type Msg
+    = Inc
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update message model =
-    case  message of
-        Inc -> (model + 1, Cmd.none)
-        
+    case message of
+        Inc ->
+            ( model + 1, Cmd.none )
+
 
 view : Model -> Html Msg
-view model = 
-       div[] [text <| toString model]
+view model =
+    div [] [ text <| toString model ]
+
 
 main =
     Html.program
         { init = init
         , update = update
         , view = view
-        , subscriptions = always Sub.none}
+        , subscriptions = always Sub.none
+        }
