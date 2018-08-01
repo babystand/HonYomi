bd3b125ca7605b2086b4240ace46cadc212649ae
Unknown <vypon3000@gmail.com>
2018-08-01 10:16:58 -0400

    started switching table to grid (need to separate header from grid/auto row)

--- HonYomi/WebUI/src/Views/LibraryView.elm
+++ HonYomi/WebUI/src/Views/LibraryView.elm
@@ -16,30 +16,27 @@ bookRow index book =
         rowClass =
             if index % 2 == 0 then
                 class "tableRow"
-
             else
                 class "tableRowAlt"
     in
-    tr [ class "book-row", rowClass ]
-        [ td [ onClick <| Playback (SetTrack <| getCurrentTrack book) ] [ i [ class "fas fa-play" ] [] ]
-        , td [] [ text <| withDefault book.guid book.title ]
-        , td [] [ text <| book.guid ]
-        , td [] [ text <| toString <| Array.length book.fileProgresses ]
-        ]
+        tr [ class "book-row", rowClass ]
+            [ div [ onClick <| Playback (SetTrack <| getCurrentTrack book) ] [ i [ class "fas fa-play" ] [] ]
+            , div [] [ text <| withDefault book.guid book.title ]
+            , div [] [ text <| book.guid ]
+            , div [] [ text <| toString <| Array.length book.fileProgresses ]
+            ]
 
 
 bookTable : Array ServerBook -> Html Msg
 bookTable books =
-    table [ class "book-table" ]
-        [ thead [ class "fixed-table-header" ]
-            [ tr []
-                [ th [] []
-                , th [] [ text "Title" ]
-                , th [] [ text "Id" ]
-                , th [] [ text "Tracks" ]
-                ]
+    div [ class "book-grid" ]
+        [ div [ class "book-grid-header" ]
+            [ div [ id "book-play-col-header" ] []
+            , div [ id "book-title-col-header" ] [ text "Title" ]
+            , div [ id "book-id-col-header" ] [ text "Id" ]
+            , div [ id "book-tracks-col-header" ] [ text "Tracks" ]
             ]
-        , tbody [ class "scrollable-table" ] (Array.toList <| Array.indexedMap bookRow books)
+        , div [ class "book-rows" ] (Array.toList <| Array.indexedMap bookRow books)
         ]
 
 
