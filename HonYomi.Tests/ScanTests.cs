29176fdd511f72a8081421243647f8bb61a18d56
Unknown <vypon3000@gmail.com>
2018-08-11 01:10:33 -0400

    basic book searching functionality

--- HonYomi.Tests/ScanTests.cs
+++ HonYomi.Tests/ScanTests.cs
@@ -25,10 +25,26 @@ namespace HonYomi.Tests
             HonyomiContext.DeleteDatabase();
             using (var db = new HonyomiContext())
             {
+                db.Database.EnsureCreated();
                 DataAccess.InsertNewBooks(books);
                 Assert.AreEqual(3, db.Books.Count());
                 Assert.IsTrue(db.Books.Include(x => x.Files).Any(x => x.Title == "A1" && x.Files.Count == 2));
             }
         }
+
+        [Test]
+        public void SearchesBooks()
+        {
+            void ShouldSearch(string input, BookInfo expected)
+            {
+                BookInfo result = BookSearch.Search(input);
+                Assert.AreEqual(expected.Title, result.Title );
+                Assert.AreEqual(expected.Author, result.Author );
+                Assert.AreEqual(expected.ISBN, result.ISBN );
+            }
+            ShouldSearch("Brandon Sanderson - Mistborn 02 - The Well of Ascension  pt 1", new BookInfo(){Title = "The Well of Ascension (Mistborn, Book 2)", Author = "Brandon Sanderson", ISBN = "9780765316882"});
+        }
+        
+        
     }
 }
--- HonYomi/Core/BookSearch.cs
+++ HonYomi/Core/BookSearch.cs
@@ -1,44 +1,44 @@
-﻿using System.Net.Http;
+﻿using System.Linq;
+using System.Net.Http;
 using Newtonsoft.Json;
 using static System.Text.RegularExpressions.Regex;
 
 namespace HonYomi.Core
 {
-    public class BookSearch
+    internal class BookSearch
     {
+        private static readonly HttpClient client = new HttpClient();
+   
 
-        public BookSearch()
-        {
-        }
-
-        private string CleanTitle(string str)
-        {
-            return str;
-        }
-        private string FormatSearch(string str)
+        private static string FormatSearch(string str)
         {
             str = str.ToLower();
-            str = Replace(str, @"[^a-z0-9\s-]", ""); // Remove all non valid chars          
-            str = Replace(str, @"\s+", " ").Trim();  // convert multiple spaces into one space  
+            str = Replace(str, @"[^a-z0-9\s-]", ""); // Remove all non valid chars      
+            str = Replace(str, @"\s0+", " "); // Replace leading zeros
+            str = Replace(str, @"\s+", " ").Trim();  // convert multiple spaces into one space
+            str = str.Replace(" -", "");
+            str = Replace(str, @"pt\s+[0-9]*","");
             str = Replace(str, @"\s", "+");          // //Replace spaces by dashes
             return str;
         }
 
-        private BookInfo Search(string filename, HttpClient client)
+        internal static BookInfo Search(string filename)
         {
             string query = FormatSearch(filename);
-            HttpResponseMessage response = client.GetAsync($"http://openlibrary.org/search.json?{query}").Result;
+            HttpResponseMessage response = client.GetAsync($"http://openlibrary.org/search.json?q={query}").Result;
             if (!response.IsSuccessStatusCode)
             {
-                return "";
-            } //TODO WARNING
+                return null;
+            } 
 
             OpenLibraryResult olr = JsonConvert.DeserializeObject<OpenLibraryResult>(response.Content.ReadAsStringAsync().Result);
-            if (olr.NumFound == 1)
+            //the only successful state
+            if (olr.NumFound >= 1)
             {
-                 BookInfo book = new BookInfo();
-                return BookInfo{Title = olr.Docs.[0]}
+                return new BookInfo {Title = olr.Docs[0].Title, Author = string.Join(", ", olr.Docs[0].AuthorName), ISBN = olr.Docs[0].Isbn.FirstOrDefault()};
             }
+
+            return null;
         }
     }
 
@@ -46,49 +46,122 @@ namespace HonYomi.Core
     {
         public string Title { get; set; }
         public string Author { get; set; }
+        public string ISBN { get; set; }
     }
-    internal  class OpenLibraryResult
+    public  class OpenLibraryResult
     {
-        public long? Start                     { get; set; }
+        [JsonProperty("start", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public long? Start { get; set; }
+
+        [JsonProperty("num_found", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
         public long? OpenLibraryResultNumFound { get; set; }
-        public long? NumFound                  { get; set; }
-        public Doc[] Docs                      { get; set; }
+
+        [JsonProperty("numFound", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public long? NumFound { get; set; }
+
+        [JsonProperty("docs", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public Doc[] Docs { get; set; }
     }
 
-    internal  class Doc
+    public  class Doc
     {
-        public string   TitleSuggest       { get; set; }
-        public string[] EditionKey         { get; set; }
-        public long?    CoverI             { get; set; }
-        public string[] Isbn               { get; set; }
-        public bool?    HasFulltext        { get; set; }
-        public string[] Text               { get; set; }
-        public string[] AuthorName         { get; set; }
-        public string[] Seed               { get; set; }
-        public long[]   Oclc               { get; set; }
-        public string[] AuthorKey          { get; set; }
-        public string[] Subject            { get; set; }
-        public string   Title              { get; set; }
-        public string[] PublishDate        { get; set; }
-        public string   Type               { get; set; }
-        public long?    EbookCountI        { get; set; }
-        public long?    EditionCount       { get; set; }
-        public string   Key                { get; set; }
-        public long[]   IdGoodreads        { get; set; }
-        public string[] Publisher          { get; set; }
-        public string[] Language           { get; set; }
-        public long?    LastModifiedI      { get; set; }
-        public long[]   IdLibrarything     { get; set; }
-        public string   CoverEditionKey    { get; set; }
-        public long[]   PublishYear        { get; set; }
-        public long?    FirstPublishYear   { get; set; }
-        public string[] Ia                 { get; set; }
-        public string   LendingIdentifierS { get; set; }
-        public string   IaCollectionS      { get; set; }
-        public string   PrintdisabledS     { get; set; }
-        public string[] IaBoxId            { get; set; }
-        public bool?    PublicScanB        { get; set; }
-        public string   LendingEditionS    { get; set; }
+        [JsonProperty("title_suggest", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public string TitleSuggest { get; set; }
+
+        [JsonProperty("edition_key", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public string[] EditionKey { get; set; }
+
+        [JsonProperty("cover_i", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public long? CoverI { get; set; }
+
+        [JsonProperty("isbn", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public string[] Isbn { get; set; }
+
+        [JsonProperty("has_fulltext", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public bool? HasFulltext { get; set; }
+
+        [JsonProperty("text", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public string[] Text { get; set; }
+
+        [JsonProperty("author_name", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public string[] AuthorName { get; set; }
+
+        [JsonProperty("seed", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public string[] Seed { get; set; }
+
+        [JsonProperty("oclc", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public long[] Oclc { get; set; }
+
+        [JsonProperty("author_key", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public string[] AuthorKey { get; set; }
+
+        [JsonProperty("subject", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public string[] Subject { get; set; }
+
+        [JsonProperty("title", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public string Title { get; set; }
+
+        [JsonProperty("publish_date", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public string[] PublishDate { get; set; }
+
+        [JsonProperty("type", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public string Type { get; set; }
+
+        [JsonProperty("ebook_count_i", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public long? EbookCountI { get; set; }
+
+        [JsonProperty("edition_count", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public long? EditionCount { get; set; }
+
+        [JsonProperty("key", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public string Key { get; set; }
+
+        [JsonProperty("id_goodreads", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+  
+        public long[] IdGoodreads { get; set; }
+
+        [JsonProperty("publisher", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public string[] Publisher { get; set; }
+
+        [JsonProperty("language", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public string[] Language { get; set; }
+
+        [JsonProperty("last_modified_i", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public long? LastModifiedI { get; set; }
+
+        [JsonProperty("id_librarything", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+  
+        public long[] IdLibrarything { get; set; }
+
+        [JsonProperty("cover_edition_key", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public string CoverEditionKey { get; set; }
+
+        [JsonProperty("publish_year", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public long[] PublishYear { get; set; }
+
+        [JsonProperty("first_publish_year", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public long? FirstPublishYear { get; set; }
+
+        [JsonProperty("ia", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public string[] Ia { get; set; }
+
+        [JsonProperty("lending_identifier_s", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public string LendingIdentifierS { get; set; }
+
+        [JsonProperty("ia_collection_s", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public string IaCollectionS { get; set; }
+
+        [JsonProperty("printdisabled_s", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public string PrintdisabledS { get; set; }
+
+        [JsonProperty("ia_box_id", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public string[] IaBoxId { get; set; }
+
+        [JsonProperty("public_scan_b", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public bool? PublicScanB { get; set; }
+
+        [JsonProperty("lending_edition_s", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public string LendingEditionS { get; set; }
     }
 
 }
